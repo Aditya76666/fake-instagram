45b35670816bd2d4485ba870fb0bb54417e442e9
Amber Smith <smithambera@gmail.com>
2016-08-02 14:31:06 -0400

    js working, beginning to style

--- rtphotos/js/main.js
+++ rtphotos/js/main.js
@@ -1,10 +1,11 @@
 var pusher,
     channel,
-    apiToken = '6e5f67bde794d28881ed'
-    // javascriptToken not their private token
+    apiToken = 'da04f5af68a3ed9b77d1',
+    photoAPI = '',
+    // photoAPI = '',
 
 // Pusher connection
-pusher = new Pusher('apiToken', {
+pusher = new Pusher(apiToken, {
   encrypted: true
 })
 
@@ -12,23 +13,48 @@ pusher = new Pusher('apiToken', {
 channel = pusher.subscribe('photos')
 channel.bind('new_photo', function(data) {
   console.log(data)
-
-  // function renderPhoto(photoObject) {
-  //
-  // }
-  //
-  // renderPhoto({photoURL: 'urlgoeshere', caption: 'Hello'})
-  //
-  // var photos = document.getElementById('photos')
-  // photos.innerHTML += photo(data.photos)
-  // window.scrollTo(0, document.body.scrollHeight)
-  // Put your code here to render incoming photos
 })
 
-fetch('back-end API to get list of existing photos goes here')
-  .then(function(data) {
-    return data.json()
-  })
-  .then(function(photos) {
-    // Put your code here to render existing photos
-  })
+function renderPhoto(photoObject) {
+  var photos = document.getElementById('photos')
+
+  var div = document.createElement('div')
+  div.classList.add('col-sm-3')
+
+  var img = document.createElement('img')
+  img.setAttribute('src', photoObject.photo)
+  div.appendChild(img)
+
+  var caption = document.createElement('h4')
+  caption.classList.add('caption')
+  caption.innerHTML = photoObject.caption
+  div.appendChild(caption)
+
+  photos.appendChild(div)
+
+}
+
+renderPhoto({photo: 'https://unsplash.it/300/?random', caption: 'Hello'})
+
+renderPhoto({photo: 'https://unsplash.it/300/?random', caption: 'Amber, you are so talented!'})
+
+renderPhoto({photo: 'https://unsplash.it/300/?random', caption: 'How do you even do it?'})
+
+renderPhoto({photo: 'https://unsplash.it/300/?random', caption: 'You are a great dog mom!'})
+
+// fetch(photoAPI)
+//
+// .then(function(data) {
+//   return data.json()
+// })
+//
+// .then(function(photos) {
+//   photos.forEach(function(photoObject) {
+//
+//     renderPhoto(photoObject)
+//   })
+// })
+//
+// .catch(function(ex) {
+//   console.log('parsing failed', ex)
+// })
