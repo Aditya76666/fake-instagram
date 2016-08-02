4e419c83d333fb16cf2f1af42e36bedffd405fb5
Amber Smith <smithambera@gmail.com>
2016-08-02 12:06:15 -0400

    photo add functions created

--- rtphotos/js/main.js
+++ rtphotos/js/main.js
@@ -1,9 +1,10 @@
 var pusher,
     channel,
     apiToken = '6e5f67bde794d28881ed'
+    // javascriptToken not their private token
 
 // Pusher connection
-pusher = new Pusher(apiToken, {
+pusher = new Pusher('apiToken', {
   encrypted: true
 })
 
@@ -11,6 +12,16 @@ pusher = new Pusher(apiToken, {
 channel = pusher.subscribe('photos')
 channel.bind('new_photo', function(data) {
   console.log(data)
+
+  // function renderPhoto(photoObject) {
+  //
+  // }
+  //
+  // renderPhoto({photoURL: 'urlgoeshere', caption: 'Hello'})
+  //
+  // var photos = document.getElementById('photos')
+  // photos.innerHTML += photo(data.photos)
+  // window.scrollTo(0, document.body.scrollHeight)
   // Put your code here to render incoming photos
 })
 
