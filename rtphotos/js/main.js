900cc4582169920a0dfe7252ff1e783ae6419c2e
Amber Smith <smithambera@gmail.com>
2016-08-02 23:04:04 -0400

    pusher working

--- rtphotos/js/main.js
+++ rtphotos/js/main.js
@@ -1,8 +1,8 @@
 var pusher,
     channel,
     apiToken = 'da04f5af68a3ed9b77d1',
-    photoAPI = 'https://80cf5e8f.ngrok.io',
-    // photoAPI = '',
+    // photoAPI = 'https://80cf5e8f.ngrok.io',
+    photoAPI = 'https://aqueous-chamber-43940.herokuapp.com/',
 
 // Pusher connection
 pusher = new Pusher(apiToken, {
@@ -12,18 +12,19 @@ pusher = new Pusher(apiToken, {
 // Pusher channel and event subscription
 channel = pusher.subscribe('photos')
 channel.bind('new_photo', function(data) {
-  console.log(data)
+console.log(data)
+renderPhoto(data)
 })
 
 function renderPhoto(photoObject) {
   var photos = document.getElementById('photos')
 
   var div = document.createElement('div')
-  div.classList.add('col-sm-3')
+  div.classList.add('grid-item')
 
   var img = document.createElement('img')
   img.classList.add('img-thumbnail')
-  img.setAttribute('src', photoAPI + photoObject.image_url)
+  img.setAttribute('src', photoObject.image_url)
   div.appendChild(img)
 
   var caption = document.createElement('h4')
@@ -34,9 +35,9 @@ function renderPhoto(photoObject) {
   photos.appendChild(div)
 }
 
-function updateGrid() {
-  var masonry = new Masonry('#photos')
-}
+// function updateGrid() {
+//   var masonry = new Masonry('#photos')
+// }
 
 fetch(photoAPI + '/photos/')
 
@@ -49,10 +50,15 @@ fetch(photoAPI + '/photos/')
   photos.forEach(function(photoObject) {
     renderPhoto(photoObject)
   })
+
   setTimeout(function() {
-    updateGrid()
+    $('.grid').masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    })
   }
-  ,0)
+  ,50)
 })
 
 .catch(function(ex) {
